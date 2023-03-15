020ce145fdb5742a8ad28c62320832da4cd3a975
RichardPMachado <richardmachado2211@gmail.com>
2023-03-15 20:38:47 -0300

    feat[requisito 26]: completo

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -3,5 +3,3 @@ db.produtos.updateMany({ "valoresNutricionais.2.percentual": { $gte: 40 } },
     $each: ["muito sódio"] } } });
 
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
-
-db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
